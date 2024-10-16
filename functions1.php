b4f628f5dcceefebad04bb51b038ab88da87e39a
Shruti <shruti.sundar0405@gmail.com>
2024-10-16 18:34:44 +0530

    birthday function

---
+++ functions1.php
@@ -0,0 +1,12 @@
+<?php
+    function happybirthday($name, $age){
+        echo "Wish you a happy birthday to {$name} <br> May all your dreams come true <br> Happy {$age} birthday dear {$name}<br> <br>";
+    }
+
+    happybirthday("Shruti", 19);
+
+    happybirthday("Shiny", 20);
+
+    happybirthday("Thendral", 40);
+
+?>
