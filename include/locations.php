3437f60c3f2d84b210f823c8b1fde9e73539adc5
shruti-s0405 <shruti.sundar0405@gmail.com>
2024-10-17 12:10:14 +0530

    include() in php

---
+++ include/locations.php
@@ -0,0 +1,21 @@
+<?php
+    include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>This is the locations page</h2>
+    <p>The contents of the locations page goes here</p>
+    <p>Thank you for seeing this website</p>
+</body>
+</html>
+
+<?php
+    include("footer.html");
+?>
