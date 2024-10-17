87313b90dd055acd6fdbc0967a4ea3548bb46b2d
shruti-s0405 <shruti.sundar0405@gmail.com>
2024-10-17 12:20:34 +0530

    added comments to include

--- include/index.php
+++ include/index.php
@@ -1,4 +1,11 @@
+
+
+
 <?php
+//include() - Copies content of a file (php/html/text)
+//and includes it in your php file.
+//Sections of our website become reusable
+//Changes only need to be made in one place
     include("header.html");
 ?>
 
